af7428be225391cad548aa2476d7c08d54df8700
Crowdbotics <team@crowdbotics.com>
2024-05-17 21:02:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lively-lake-48011",
+  url: "https://lively-lake-48011.botics.co",
+  api: "https://lively-lake-48011.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
